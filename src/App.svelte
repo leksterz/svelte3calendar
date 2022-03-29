89bc2f61bf67bfa815a86d0be238fe8514638c7f
Alex Ablazevics <ablazevix@gmail.com>
2022-03-28 23:03:00 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import DatePicker from "./DatePicker.svelte";
+
+	// has current date
+  let currentDate = new Date();
+	
+	// 
+  const onDateChange = d => {
+    currentDate = d.detail;
+  };
+</script>
+
+<h1>svelte-dates</h1>
+<!-- 
+DatePicker component
+Upstream Event - on:datechange (executes onDateChange)
+Param 1 - currentDate param (based on current date)
+
+-->
+<DatePicker
+  on:datechange={onDateChange}
+  selected={currentDate}
+  isAllowed={date => {
+    const millisecs = date.getTime();
+    if (millisecs + 25 * 3600 * 1000 < Date.now()) return false;
+    if (millisecs > Date.now() + 3600 * 24 * 45 * 1000) return false;
+    return true;
+  }} />
+
+<!--
+  // setInterval(() => {
+  //   currentDate = new Date(currentDate.getTime() + 24 * 3600 * 1000);
+  // }, 1000);
+
+  // const isAllowed = date => {
+  //   const millisecs = date.getTime();
+  //   if (millisecs + 25 * 3600 * 1000 < Date.now()) return false;
+  //   if (millisecs > Date.now() + 3600 * 24 * 30 * 1000) return false;
+  //   return true;
+  // };
+-->
